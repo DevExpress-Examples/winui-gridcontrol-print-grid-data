20b54c9fca312524896e8b407b0ac4bf78dbd370
Anton Sameishchev <anton.sameishchev@devexpress.com>
2021-10-30 14:02:27 +0300

    Code added

---
+++ CS/PrintGridControl/PrintGridControl/Views/GridPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using DevExpress.WinUI.Grid.Printing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace PrintGridControl {
+
+    public sealed partial class GridPage : Page {
+
+        public GridPage() {
+            InitializeComponent();
+        }
+
+        public ViewModel ViewModel { get; } = new ViewModel();
+
+        void Button_Click(object sender, RoutedEventArgs e) {
+            gridcontrol.Print();
+        }
+    }
+}
